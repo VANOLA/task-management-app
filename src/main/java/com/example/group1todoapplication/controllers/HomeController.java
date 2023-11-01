6c5e94d606f6390858a3d04c54a7259ae649caa1
VANOLA <62918869+VANOLA@users.noreply.github.com>
2023-11-01 10:26:20 -0500

    deleted categories and made table searchable

--- src/main/java/com/example/group1todoapplication/controllers/HomeController.java
+++ src/main/java/com/example/group1todoapplication/controllers/HomeController.java
@@ -1,16 +1,10 @@
 package com.example.group1todoapplication.controllers;
 
-import com.example.group1todoapplication.models.TodoItem;
 import com.example.group1todoapplication.services.TodoItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Comparator;
-import java.util.List;
 
 /**
  * This controller provides access to the applications home page. It interprets
@@ -28,21 +22,12 @@ public class HomeController {
      * Handler method gets task from the service and passes it to the
      * ModelAndView object. It assigns it a key, and makes it available
      * to the index.html file.
-     * @return
+     * @return returns model and view
      */
     @GetMapping("/")
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("todoItems", todoItemService.getAll());
         return modelAndView;
-    }
-
-    @GetMapping("/sort/{id}")
-    public String showSortByDueDate(@PathVariable("id") Long id,  Model model) {
-        List<TodoItem> todoItems = (List<TodoItem>) todoItemService.getAllIterable();
-        todoItems.sort(Comparator.comparing(TodoItem::getDueDate));
-
-        model.addAttribute("todoItems", todoItems);
-        return "index";
     }
 }
