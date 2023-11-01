5bf9ca3febd11ca74d02b7273c26798481fbc397
John Abend <johnabend@Johns-MacBook-Air.local>
2023-10-31 20:43:33 -0400

    fix the logic for today variable

--- src/main/java/com/example/group1todoapplication/controllers/TodayController.java
+++ src/main/java/com/example/group1todoapplication/controllers/TodayController.java
@@ -1,5 +1,6 @@
 package com.example.group1todoapplication.controllers;
 
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,11 +19,13 @@ import com.example.group1todoapplication.services.TodoItemService;
 public class TodayController {
      @Autowired
     private TodoItemService todoItemService;
+    private LocalDate today = LocalDate.now();
 
     @GetMapping("/today")
     public String showSortByCategoryPage(Model model) {
-        List<TodoItem> todoItems = StreamSupport.stream(todoItemService.getAll().spliterator(),
-                false).collect(Collectors.toList());
+        List<TodoItem> todoItems = StreamSupport.stream(todoItemService.getAll().spliterator(), false)
+            .filter(item -> item.getDueDate() != null && item.getDueDate().equals(today))
+            .collect(Collectors.toList());
         todoItems.sort(Comparator.comparing(TodoItem::getTaskCategory));
 
         model.addAttribute("todoItems", todoItems);
